688e81ead487b3837609d38b1be63fd713f2cbc7
Pratik Shah <Pratik-Shah@users.noreply.github.com>
2021-11-10 13:52:55 +0530

    Update DeploymentScript.ps1
    fixed  ingestion mapping syntax

--- DeploymentScript.ps1
+++ DeploymentScript.ps1
@@ -8,7 +8,7 @@ param(
 Write-Host $clusterUrl
 Write-Host $dbName
 Write-Host $logsTableName
-
+Write-Host $logsTableNameIngestionMappingName
 
 
 $token=(Get-AzAccessToken -ResourceUrl $clusterUrl).Token
@@ -18,31 +18,23 @@ $header = @{
  "Content-Type"="application/json"
 }
 
-
 $body = @{
  "db"="$dbName"
  "csl"=".create table [$logsTableName]  (['log']: string, ['stream']: string, docker: dynamic, kubernetes: dynamic, fluentd_time: long, host: string, namespace_name: string, pod_name: string, container_name: string, fluentd_datetime: datetime)"
 } | ConvertTo-Json
 $result = Invoke-RestMethod -Uri "$clusterUrl/v1/rest/mgmt" -Method 'Post' -Body $body -Headers $header 
 
+$csl = ".create table $($logsTableName) ingestion json mapping '$($logsTableNameIngestionMappingName)' '[{ ""column"" : ""log"", ""Properties"":{""Path"":""$.log""}}, { ""column"" : ""stream"", ""Properties"":{""Path"":""$.stream""}},	{ ""column"" : ""docker"", ""Properties"":{""Path"":""$.docker""}}, { ""column"" : ""kubernetes"", ""Properties"":{""Path"":""$.kubernetes""}},	{ ""column"" : ""fluentd_time"", ""Properties"":{""Path"":""$.fluentd_time""}},	{ ""column"" : ""host"", ""Properties"":{""Path"":""$.kubernetes.host""}},	{ ""column"" : ""namespace_name"", ""Properties"":{""Path"":""$.kubernetes.namespace_name""}}, 	{ ""column"" : ""pod_name"",""Properties"":{""Path"":""$.kubernetes.pod_name""}}, 	{ ""column"" : ""container_name"", ""Properties"":{""Path"":""$.kubernetes.container_name""}}, 	{ ""column"" : ""fluentd_datetime"",""Properties"":{""Path"":""$.fluentd_time"",""Transform"":""DateTimeFromUnixMilliseconds""}} ]'"
+Write-Host $csl
+
 $body = @{
     "db"="$dbName"
-    "csl"='.create table [$logsTableName] ingestion json mapping "[$logsTableNameIngestionMappingName]"
-    "["
-" 	{ "column" : "log", "Properties":{"Path":"$.log"}},"
-"	{ "column" : "stream", "Properties":{"Path":"$.stream"}},"
-"	{ "column" : "docker", "Properties":{"Path":"$.docker"}},"
-"	{ "column" : "kubernetes", "Properties":{"Path":"$.kubernetes"}},"
-"	{ "column" : "fluentd_time", "Properties":{"Path":"$.fluentd_time"}},"
-"	{ "column" : "host", "Properties":{"Path":"$.kubernetes.host"}},"
-"	{ "column" : "namespace_name", "Properties":{"Path":"$.kubernetes.namespace_name"}},"
-"	{ "column" : "pod_name", "Properties":{"Path":"$.kubernetes.pod_name"}},"
-"	{ "column" : "container_name", "Properties":{"Path":"$.kubernetes.container_name"}},"
-"	{ "column" : "fluentd_datetime", "Properties":{"Path":"$.fluentd_time","Transform":"DateTimeFromUnixMilliseconds"}}"
-"]" '
+    "csl"=$csl
 } | ConvertTo-Json
+Write-Host $body
+
 $result = Invoke-RestMethod -Uri "$clusterUrl/v1/rest/mgmt" -Method 'Post' -Body $body -Headers $header 
-   
+      
 
 $body = @{
  "db"="$dbName"
